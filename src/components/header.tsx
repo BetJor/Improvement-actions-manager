638e96fd6071f7868e77ef9fe8b075b35dfb49b2
Elisabet Jordana Quer <ejordana@costaisa.com>
2025-07-30 13:20:32 +0000

    Necessito que l'usuari trii l'idioma a COnfiguracio

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { CircleUser, Menu, Search } from "lucide-react"
+import { CircleUser, Menu } from "lucide-react"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,17 +8,27 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { AppSidebar } from "./app-sidebar"
-import Link from "next/link"
 import { useTranslations } from "next-intl"
+import { LanguageSwitcher } from "./language-switcher"
 
 export function Header() {
   const t = useTranslations('Header');
+  const tDialog = useTranslations('SettingsDialog');
+
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-card px-4 lg:h-[60px] lg:px-6 sticky top-0 z-30">
+      <Dialog>
         <Sheet>
             <SheetTrigger asChild>
                 <Button
@@ -35,27 +45,40 @@ export function Header() {
             </SheetContent>
         </Sheet>
 
-      <div className="w-full flex-1">
-        <h1 className="font-semibold text-lg">
-            {t("title")}
-        </h1>
-      </div>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="secondary" size="icon" className="rounded-full">
-            <CircleUser className="h-5 w-5" />
-            <span className="sr-only">Toggle user menu</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>{t("myAccount")}</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>{t("settings")}</DropdownMenuItem>
-          <DropdownMenuItem>{t("support")}</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>{t("logout")}</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+        <div className="w-full flex-1">
+          <h1 className="font-semibold text-lg">
+              {t("title")}
+          </h1>
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon" className="rounded-full">
+              <CircleUser className="h-5 w-5" />
+              <span className="sr-only">Toggle user menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>{t("myAccount")}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DialogTrigger asChild>
+              <DropdownMenuItem>{t("settings")}</DropdownMenuItem>
+            </DialogTrigger>
+            <DropdownMenuItem>{t("support")}</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>{t("logout")}</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <DialogContent>
+            <DialogHeader>
+              <DialogTitle>{tDialog("title")}</DialogTitle>
+              <DialogDescription>
+                {tDialog("description")}
+              </DialogDescription>
+            </DialogHeader>
+            <LanguageSwitcher />
+          </DialogContent>
+      </Dialog>
     </header>
   )
 }
---
+++ src/components/language-switcher.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { usePathname, useRouter } from "next-intl/client";
+import { useLocale, useTranslations } from "next-intl";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+
+export function LanguageSwitcher() {
+  const t = useTranslations("LanguageSwitcher");
+  const locale = useLocale();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const onSelectChange = (value: string) => {
+    router.replace(pathname, { locale: value });
+  };
+
+  return (
+    <div className="grid gap-2">
+      <Label htmlFor="language-select">{t("selectLanguage")}</Label>
+      <Select defaultValue={locale} onValueChange={onSelectChange}>
+        <SelectTrigger id="language-select">
+          <SelectValue placeholder={t("selectLanguage")} />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="ca">{t("catalan")}</SelectItem>
+          <SelectItem value="es">{t("spanish")}</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
