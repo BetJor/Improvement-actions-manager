6ca9cdffe8328da9486251e2977c2ef00a4000d6
Elisabet Jordana Quer <ejordana@costaisa.com>
2025-07-31 11:38:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/[locale]/prompt-gallery/page.tsx
+++ src/app/[locale]/prompt-gallery/page.tsx
@@ -271,8 +271,8 @@ export default function PromptGalleryPage() {
                                 </TableRow>
                             ) : prompts.length > 0 ? (
                                 prompts.map((p) => (
-                                    <Collapsible key={p.id} asChild>
-                                        <Fragment>
+                                    <Collapsible asChild key={p.id} >
+                                        <>
                                             <TableRow>
                                                 <TableCell>
                                                      <CollapsibleTrigger asChild>
@@ -329,7 +329,7 @@ export default function PromptGalleryPage() {
                                                     </TableCell>
                                                 </TableRow>
                                             </CollapsibleContent>
-                                        </Fragment>
+                                        </>
                                     </Collapsible>
                                 ))
                             ) : (
