74a30f7bc3155e372123c2a9e2deaf43dde91e26
Elisabet Jordana Quer <ejordana@costaisa.com>
2025-08-11 11:12:41 +0000

    Res, igual, Access to XMLHttpRequest at 'https://firebasestorage.googlea

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   "projectId": "improvement-actions-manager",
   "appId": "1:920139375274:web:3c4fd3616765e7480566d3",
-  "storageBucket": "improvement-actions-manager.appspot.com",
+  "storageBucket": "improvement-actions-manager.firebasestorage.app",
   "apiKey": "AIzaSyDb99QMKUYzyWyajTwZ2plmEB1o35g_9H4",
   "authDomain": "improvement-actions-manager.firebaseapp.com",
   "messagingSenderId": "920139375274"
