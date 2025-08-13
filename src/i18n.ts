2bef94ab937cb106d4912ef9b5032d5d04affbf9
Your Name <ejordana@costaisa.com>
2025-08-13 06:53:16 +0000

    Si, pertò aquests dos fitxers s'estan fent molt grossos i son lents de r

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,15 +1,12 @@
-import {notFound} from 'next/navigation';
-import {getRequestConfig} from 'next-intl/server';
- 
-// Can be imported from a shared config
+import { getRequestConfig } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+
 const locales = ['ca', 'es'];
- 
-export default getRequestConfig(async ({locale}) => {
-  // Validate that the incoming `locale` parameter is valid
+
+export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale as any)) notFound();
- 
+
   return {
     messages: (await import(`../messages/${locale}.json`)).default,
-    locale,
   };
 });
