515730d1cb083f30605e58fe25503d140e40e5d7
Elisabet Jordana Quer <ejordana@costaisa.com>
2025-08-29 08:10:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,26 +3,28 @@ import "./globals.css"
 import { Inter } from 'next/font/google'
 import { AuthProvider } from "@/hooks/use-auth"
 import { Toaster } from "@/components/ui/toaster"
-import { getLocale } from "next-intl/server"
+import { getMessages, getLocale } from "next-intl/server"
+import { NextIntlClientProvider } from "next-intl"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default async function RootLayout({
   children,
-  params,
 }: {
   children: React.ReactNode,
-  params: { locale: string }
 }) {
   const locale = await getLocale();
+  const messages = await getMessages();
 
   return (
     <html lang={locale}>
       <body className={inter.className}>
-        <AuthProvider>
-          {children}
-          <Toaster />
-        </AuthProvider>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <AuthProvider>
+            {children}
+            <Toaster />
+          </AuthProvider>
+        </NextIntlClientProvider>
       </body>
     </html>
   )
