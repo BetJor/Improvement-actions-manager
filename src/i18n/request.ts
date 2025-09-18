6113d2e87ca9ba5f560a259ed5d6dc3b268cdd68
Elisabet Jordana Quer <ejordana@costaisa.com>
2025-09-18 08:15:20 +0000

    Try fixing this error: `Console Error: A `locale` is expected to be retu

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,11 +1,10 @@
 
-import {getRequestConfig, getLocale} from 'next-intl/server';
+import {getRequestConfig} from 'next-intl/server';
 import {notFound} from 'next/navigation';
  
 const locales = ['ca', 'es'];
  
-export default getRequestConfig(async () => {
-  const locale = await getLocale();
+export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
  
