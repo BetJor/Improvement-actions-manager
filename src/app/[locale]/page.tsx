ccf5bccd6e4d2cb74292d2bb824b23fac803c392
Elisabet Jordana Quer <ejordana@costaisa.com>
2025-09-23 07:21:58 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/[locale]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-import {unstable_setRequestLocale} from 'next-intl/server';
-
-export default function RootPage({params: {locale}}: {params: {locale: string}}) {
-  unstable_setRequestLocale(locale);
-  redirect('/dashboard');
-}
